ea407d7bf1a7d94739a5683bf0e49e9a12917a55
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-10-28 21:50:18 +0800

    改用 new 的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-\App\Post::create([
-    'title'=>'test title',
-    'content'=>'test content',
-]);
+$post = new \App\Post();
+$post->title = 'test title';
+$post->content = 'test content';
+$post->save();
 
 
 protected $fillable = [

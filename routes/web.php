69a811514828a125eb3e3b580808a3701bf7833e
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-01 23:33:05 +0800

    錯誤修正

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
     \App\Post::create([
-        'title'=>'testtitle',
-        'content'=>'testcontent',
+        'title'=>'test title',
+        'content'=>'test content',
     ]);
 });
 
